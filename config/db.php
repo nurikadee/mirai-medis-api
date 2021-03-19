854b6db598e95c10a0cafd6968a52d2caed499ca
Nurika Dewi <nurikadee@gmail.com>
2021-03-19 16:42:47 +0700

    fix: Remove path web on base url

--- config/db.php
+++ config/db.php
@@ -1,8 +1,14 @@
 <?php
 return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'pgsql:host=127.0.0.1;dbname=simrs',
+    // 'username' => 'nurikadewi',
+    // 'password' => 'nurikadewi',
+    // 'charset' => 'utf8',
+
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=127.0.0.1;dbname=simrs',
-    'username' => 'nurikadewi',
-    'password' => 'nurikadewi',
+    'dsn' => 'pgsql:host=192.168.254.70;port=5432;dbname=simrs',
+    'username' => 'postgres',
+    'password' => '1satu2dua',
     'charset' => 'utf8',
 ];
