b08d53d60b387488d8c7650ab642398da1c12bf8
Nurika Dewi <nurikadee@gmail.com>
2021-03-26 10:53:17 +0700

    fea: Get Poli by Id Poli BPJS

--- modules/v1/controllers/PoliController.php
+++ modules/v1/controllers/PoliController.php
@@ -41,4 +41,33 @@ class PoliController extends Controller
 
         return ResponseHelper::success(Status::STATUS_OK, "Successfully", $poliList);
     }
+
+    public function actionGetPoliRsByPoliBpjs()
+    {
+        $params = Yii::$app->request->post();
+        $poli_bpjs_id = $params['poli_bpjs_id'];
+
+        $poli = Poli::findAllPoliRsByPoliBpjs($poli_bpjs_id);
+
+        $poliGroup = [];
+        $poliList = [];
+        foreach ($poli as $value) {
+            $poli_rs = [
+                "poli_rs_id" => $value["poli_rs_id"],
+                "poli_rs_nama" => $value["poli_rs_nama"],
+            ];
+
+            $poliGroup[$value["poli_bpjs_id"]]["poli_bpjs_id"] = $value["poli_bpjs_id"];
+            $poliGroup[$value["poli_bpjs_id"]]["poli_bpjs_nama"] = $value["poli_bpjs_nama"];
+            $poliGroup[$value["poli_bpjs_id"]]["poli_rs"][] = $poli_rs;
+        }
+
+        foreach ($poliGroup as $poli) {
+            if ($poli["poli_bpjs_nama"] != null) {
+                $poliList = $poli;
+            }
+        }
+
+        return ResponseHelper::success(Status::STATUS_OK, "Successfully", $poliList);
+    }
 }
